1da6c8e614a17a7cc03c523449eff25f32dda816
Remmy Abubakar <remmybello123@gmail.com>
2023-04-08 09:39:55 +0000

    image index

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from "react-icons/bs";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const images = [
   {
@@ -67,16 +69,66 @@ const images = [
 ];
 
 function ImageGallery() {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+  const toggleModal = (index) => setSelectedImageIndex(index);
+
+  const navigate = (direction) => {
+    setSelectedImageIndex((prevIndex) =>
+      direction === "next"
+        ? (prevIndex + 1) % images.length
+        : (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+
+  const selectImage = (index) => {
+    setSelectedImageIndex(index);
+  };
+
+  
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 justify-center">
         {images.map((image, index) => (
           <img
+            key={index}
             src={image.img}
+            alt={image.alt}
+            onClick={() => selectImage(index)}
             className="lg:h-96 md:h-96 sm:h-52 w-[100%] rounded-2xl object-cover"
           />
         ))}
       </div>
+
+      {selectedImageIndex !== null && (
+        <div className="modalBackground fixed flex justify-center items-center inset-0 h-[100%] bg-neutral-900 bg-opacity-90 w-[100vw] cursor-pointer">
+          <button
+            onClick={() => navigate("prev")}
+            className="absolute lg:mr-[60rem] md:mr-[40rem] sm:mr-[21rem] sm:mt-20"
+          >
+            <BsArrowLeftCircleFill color="white" className="text-5xl" />
+          </button>
+          <button
+            onClick={() => navigate("next")}
+            className="absolute lg:ml-[60rem] md:ml-[40rem] sm:ml-[21rem] sm:mt-20"
+          >
+            <BsArrowRightCircleFill color="white" className="text-5xl" />
+          </button>
+          <button
+            onClick={() => toggleModal(null)}
+            className="absolute lg:mb-[35rem] md:mb-[35rem] sm:mb-96 lg:ml-[80rem] md:ml-[33rem] sm:ml-80"
+          >
+            <IoMdCloseCircle color="white" className="text-5xl" />
+          </button>
+          <div className="rounded-2xl lg:p-10 md:p-10 sm:p-7">
+            <img
+              src={images[selectedImageIndex].img}
+              alt={images[selectedImageIndex].alt}
+              className="lg:h-[40rem] md:h-[40rem] sm:h-[30rem] w-[100%] mt-20 rounded-2xl object-cover"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
